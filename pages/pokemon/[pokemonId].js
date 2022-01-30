41cebcc8cf5b632c43a9967841c19c6eb88cc661
Drlazinho <lazbonfim@hotmail.com>
2022-01-30 12:30:42 -0300

    Card- details

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,4 +1,8 @@
-export const getStaticPaths = async() => {
+import Image from 'next/image'
+
+import styles from '../../styles/Pokemon.module.css'
+
+export const getStaticPaths = async () => {
   const maxPokemons = 300
   const api = 'https://pokeapi.co/api/v2/pokemon'
 
@@ -14,24 +18,61 @@ export const getStaticPaths = async() => {
     }
   })
 
-  return{
+  return {
     paths,
-    fallback:false,
+    fallback: false
   }
 }
 
-export const getStaticProps = async(context) =>{
+export const getStaticProps = async context => {
   const id = context.params.pokemonId
 
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
   const data = await res.json()
 
-  return{
-    props: {pokemon: data},
+  return {
+    props: { pokemon: data }
   }
 }
 
-export default function Pokemon({pokemon}) {
-  return <p>{pokemon.name}</p>
+export default function Pokemon({ pokemon }) {
+  return (
+    <div className={styles.pokemon_container}>
+      <h1 className={styles.title}>{pokemon.name}</h1>
+      <Image
+        src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+        width="200px"
+        height="200px"
+        alt={pokemon.name}
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+      <div>
+        <h3>Tipo:</h3>
+        <div className={styles.types_container}>
+          {pokemon.types.map((item, index) => (
+            <span
+              key={index}
+              className={`${styles.type} ${styles['type_' + item.type.name]}`}
+            >
+              {item.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className={styles.data_container}>
+        <div className={styles.data_height}>
+          <h4>Altura:</h4>
+          <p>{pokemon.height * 10} cm</p>
+        </div>
+        <div className={styles.data_weight}>
+          <h4>Peso:</h4>
+          <p>{pokemon.weight / 10} kg</p>
+        </div>
+      </div>
+    </div>
+  )
 }
